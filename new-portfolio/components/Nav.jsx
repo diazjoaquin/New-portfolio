31de83ee1fade63661c829a9dd2cebf7d7d06cca
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-06-17 23:51:54 -0300

    change text color

--- new-portfolio/components/Nav.jsx
+++ new-portfolio/components/Nav.jsx
@@ -2,16 +2,16 @@ import { BiBong } from "react-icons/bi";
 
 export default function Nav () {
     return (
-        <nav className="w-full h-20 fixed text-white flex items-center font-mono text-l uppercase z-0">
+        <nav className="w-full h-20 fixed text-gray-300 flex items-center font-mono text-l uppercase z-0">
             <div className="flex  items-center w-7/12 space-x-5 mx-5">
                 <BiBong className="text-5xl"/>
                 <h3 className="font-bold text-2xl hover:cursor-pointer">Joaquín Díaz</h3>
             </div>
             <div className="w-5/12 flex">
                 <ul className="w-full flex justify-evenly">
-                    <li className="hover:underline hover:cursor-pointer">About</li>
-                    <li className="hover:underline hover:cursor-pointer">Portfolio</li>
-                    <li className="hover:underline hover:cursor-pointer">Contact</li>
+                    <li className="hover:underline hover:cursor-pointer hover:text-white">About</li>
+                    <li className="hover:underline hover:cursor-pointer hover:text-white">Portfolio</li>
+                    <li className="hover:underline hover:cursor-pointer hover:text-white">Contact</li>
                 </ul>
             </div>
         </nav>
