7a8d47bfdb0894a4fb55611ddbaba9076cc80de5
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-07-10 22:07:25 -0300

    ft: Hide Contact Sidebar on sm devices

--- new-portfolio/components/ContactSidebar.jsx
+++ new-portfolio/components/ContactSidebar.jsx
@@ -3,7 +3,7 @@ import { FaTelegramPlane } from 'react-icons/fa';
 
 const ContactSidebar = () => {
     return (
-        <aside className="w-[20%] h-[50%] text-gray-200 text-3xl flex flex-col justify-between">
+        <aside className="w-[20%] h-[50%] text-gray-200 text-3xl flex flex-col justify-between hidden lg:flex">
             <a className="hover:scale-125 hover:text-white transition ease-in-out delay-150" href="https://wa.me/+541126792759"><AiOutlineWhatsApp/></a>
             <a className="hover:scale-125 hover:text-white transition ease-in-out delay-150" href="mailto:diaz.joaquin.e@gmail.com"><AiOutlineMail/></a>
             <a className="hover:scale-125 hover:text-white transition ease-in-out delay-150" href="https://t.me/diazjoaquin"><FaTelegramPlane/></a>
