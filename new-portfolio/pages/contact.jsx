cbf9a0653a94e741ef6e0b4be86e5a7f8efbe7cf
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-07-29 00:05:56 -0300

    style: change styles on submit button

--- new-portfolio/pages/contact.jsx
+++ new-portfolio/pages/contact.jsx
@@ -7,14 +7,13 @@ const Contact = () => {
 
     return (
         <section className="h-full">
-            <div className="absolute top-0 bottom-0 right-0 left-0 bg-zinc-800 z-0 skew-y-[-6deg] origin-top-left"></div>
+            <div className="absolute top-0 bottom-0 right-0 left-0 bg-zinc-900 z-0 skew-y-[-3deg] origin-top-left"></div>
             <Nav/>
             <Sidebar/>
             <main className="flex h-full justify-center items-center">
-                <div className="h-[75%] w-[65%] gap-10 flex flex-col justify-center items-center absolute rounded-3xl lg:p-12 bg-zinc-900 animate-fade-down animate-once animate-duration-[800ms] animate-delay-500 animate-ease-in">
-                    <div className="text-white flex flex-col items-center gap-1 mt-12">
-                        <h2 className="text-4xl">Get in Touch</h2>
-                        <h3 className="text-xl">Contact Me</h3>
+                <div className="h-[70%] w-[60%] gap-10 flex flex-col justify-center items-center absolute rounded-3xl lg:p-12 animate-fade-down animate-once animate-duration-[800ms] animate-delay-500 animate-ease-in border-4">
+                    <div className="text-white flex flex-col mt-12">
+                        <h2 className="text-4xl">Get in Touch!</h2>
                     </div>
                     <div className="flex w-[60%]">
                         <ContactSidebar/>
@@ -23,7 +22,7 @@ const Contact = () => {
                                 <input className="rounded-xl px-3 py-1 w-[100%]" type='text' placeholder='Your Name' required />
                                 <input className="rounded-xl px-3 py-1 w-[100%]" type="email" placeholder='Your Email' required />
                                 <textarea className="rounded-xl px-3 py-1 w-[100%] resize-none" name="Message" rows="10" placeholder='Your Message' required ></textarea>
-                                <button className="text-white bg-zinc-800 hover:bg-zinc-700 px-6 py-2 rounded-xl font-bold" type='submit'>Submit</button>
+                                <button className="text-white bg-zinc-700 hover:bg-zinc-600 px-6 py-2 rounded-xl font-bold border-2" type='submit'>Submit</button>
                             </form>
                         </div>
                     </div>
