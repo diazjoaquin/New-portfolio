1e6ed64a111119cb20a91fc1f8844a5a5edc7756
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-17 16:26:18 -0300

    style: change font style on contact page

--- new-portfolio/pages/contact.jsx
+++ new-portfolio/pages/contact.jsx
@@ -1,34 +1,32 @@
 import Nav from "@/components/Nav";
 import Sidebar from "@/components/Sidebar";
-import ContactSidebar from "@/components/ContactSidebar";
-import Pages from "@/components/Pages";
+// import ContactSidebar from "@/components/ContactSidebar";
 
 const Contact = () => {
 
     return (
-        <section className="h-full">
-            <div className="absolute top-0 bottom-0 right-0 left-0 bg-zinc-900 z-0 skew-y-[-3deg] origin-top-left"></div>
+        <section className="h-full bg-zinc-900">
+            {/* <div className="absolute bottom-0 w-full h-[42%] bg-emerald-600 origin-bottom z-10 rounded-tr-[56%] rounded-tl-[65%]"></div> */}
             <Nav/>
             <Sidebar/>
             <main className="flex h-full justify-center items-center">
-                <div className="h-[70%] w-[60%] gap-10 flex flex-col justify-center items-center absolute rounded-3xl lg:p-12 animate-fade-down animate-once animate-duration-[800ms] animate-delay-500 animate-ease-in border-4">
+                <div className="z-10 bg-zinc-900 h-[70%] w-[60%] gap-10 flex flex-col justify-center items-center absolute rounded-xl lg:p-12 animate-fade-down animate-once animate-duration-[800ms] animate-delay-500 animate-ease-in border-4 border-zinc-800">
                     <div className="text-white flex flex-col mt-12">
-                        <h2 className="text-4xl">Get in Touch!</h2>
+                        <h2 className="text-4xl font-serif">Get in Touch!</h2>
                     </div>
-                    <div className="flex w-[60%]">
-                        <ContactSidebar/>
-                        <div className="flex justify-center w-[100%]">
-                            <form className="flex flex-col items-center justify-center gap-5 w-[100%] mb-12">
+                    <div className="lg:flex justify-center w-[80%]">
+                        {/* <ContactSidebar/> */}
+                        <div className="w-[80%]">
+                            <form className="flex flex-col items-center justify-center gap-5 w-[100%] mb-12 font-sans">
                                 <input className="rounded-xl px-3 py-1 w-[100%]" type='text' placeholder='Your Name' required />
                                 <input className="rounded-xl px-3 py-1 w-[100%]" type="email" placeholder='Your Email' required />
                                 <textarea className="rounded-xl px-3 py-1 w-[100%] resize-none" name="Message" rows="10" placeholder='Your Message' required ></textarea>
-                                <button className="text-white bg-zinc-700 hover:bg-zinc-600 px-6 py-2 rounded-xl font-bold border-2" type='submit'>Submit</button>
+                                <button className="w-full lg:w-[40%] text-white text-lg bg-emerald-600 hover:bg-emerald-500 px-6 py-2 rounded-md font-bold border-2 border-white" type='submit'>Submit</button>
                             </form>
                         </div>
                     </div>
                 </div>
             </main>
-            <Pages/>
         </section>
     )
 };
