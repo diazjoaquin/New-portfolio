28fc5e816564dfef48fb7c482bd4c9910ec7c62c
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-17 17:12:56 -0300

    style: change palette color on navbar

--- new-portfolio/components/Nav.jsx
+++ new-portfolio/components/Nav.jsx
@@ -29,21 +29,21 @@ export default function Nav () {
     return (
         <nav className="w-full h-20 fixed text-white flex items-center font-mono text-l uppercase z-10">
             <div className="flex  items-center lg:w-7/12 lg:space-x-5 mx-5">
-                <BiBong className="text-5xl text-emerald-500 drop-shadow-[1px_1px_1.5px_#22c55e] animate-wiggle animate-infinite animate-delay-0 animate-ease-linear"/>
+                <BiBong className="text-5xl text-teal-400 drop-shadow-[1px_1px_1.5px_#22c55e] animate-wiggle animate-infinite animate-delay-0 animate-ease-linear"/>
                 <h3 className="font-bold text-2xl text-gray-300 hover:cursor-pointer hover:text-white">Joaquín Díaz</h3>
             </div>
             <div className="w-5/12 hidden lg:flex">
                 <ul className="w-full flex justify-evenly hover:text-gray-300 py-2">
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/home" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-emerald-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/home" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
                         <Link href="/home">Home</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/about" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-emerald-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/about" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
                         <Link href="/about">About</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/portfolio" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-emerald-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/portfolio" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
                         <Link href="/portfolio">Portfolio</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/contact" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-emerald-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/contact" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
                         <Link href="/contact">Contact</Link>
                     </li>
                 </ul>
