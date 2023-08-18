45b7a29aef1bbe96870a49337bdd1f08b640fab3
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-18 00:54:13 -0300

    refactor: change on how testimonials are showed

---
+++ new-portfolio/assets/testimonials.js
@@ -0,0 +1,41 @@
+export const testimonials = [
+    {
+        id: "testimonial-1",
+        img: "/images/testimonials/Santiago.jpg",
+        github: "https://github.com/SantiRosso",
+        linkedIn: "https://www.linkedin.com/in/santiago-rosso-421484227/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        id: "testimonial-2",
+        img: "/images/testimonials/Franco.jpg",
+        github: "https://github.com/Fran3003",
+        linkedIn: "https://www.linkedin.com/in/franco-emanuel-rantucho-3822a3209/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+
+    {
+        id: "testimonial-3",
+        img: "/images/testimonials/Johan.png",
+        github: "https://github.com/Zetah07",
+        linkedIn: "https://www.linkedin.com/in/Zetahdev/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        id: "testimonial-4",
+        img: "/images/testimonials/Paul.jpg",
+        github: "https://github.com/PaulBlack91/PaulBlack91",
+        linkedIn: "https://www.linkedin.com/in/paul-yasser-esperon-035a94221/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    // {
+    //     id: "testimonial-5",
+    //     img: "",
+    //     github: "",
+    //     linkedIn: "",
+    //     decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    // }
+]
--- new-portfolio/components/Testimonials.jsx
+++ new-portfolio/components/Testimonials.jsx
@@ -1,46 +1,39 @@
+import { useState } from "react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { FiHexagon } from 'react-icons/fi';
+import { testimonials } from "@/assets/testimonials";
 
 export default function Testimonials () {
 
+    const [ testimonialsPerPage ] = useState(2);
+    const [ currentPage, setCurrentPage ] = useState(1);
+    const last = currentPage * testimonialsPerPage;
+    const first = last - testimonialsPerPage;
+    const currentTestimonials = testimonials.slice(first, last); 
 
     return (
             <main className="border-4 border-zinc-700 rounded-xl sm:w-8/12 lg:w-7/12 h-[80%] flex flex-col items-center justify-start pt-6 gap-24 animate-fade-down animate-duration-[1200ms] animate-ease-in">
-                <div className="lg:w-8/12 h-2/6">
-                    <div className="flex items-end text-white text-5xl justify-center gap-4 py-2">
-                        <a href="https://github.com/SantiRosso" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillGithub/></a>
-                        <img src="/images/testimonials/Santiago.jpg" className="rounded-full object-center object-cover w-2/12 hover:scale-125" alt="Santiago" />
-                        <a href="https://www.linkedin.com/in/santiago-rosso-421484227/" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillLinkedin/></a>
-                    </div>
-                    <div className="px-6">
-                        <p className="text-white text-md text-center">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                    </div>
-                </div>
-                <div className="lg:w-8/12 h-2/6">
-                    <div className="flex items-end text-white text-5xl justify-center gap-4 py-2">
-                        <a href="https://github.com/Fran3003" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillGithub/></a>
-                        <img src="/images/testimonials/Franco.jpg" className="rounded-full object-center object-cover w-2/12 hover:scale-125" alt="Franco" />
-                        <a href="https://www.linkedin.com/in/franco-emanuel-rantucho-3822a3209/" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillLinkedin/></a>
-                    </div>
-                    <div className="px-6">
-                        <p className="text-white text-md text-center">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  
-                        </p>
-                    </div>
-                </div>
-                {/* <div className="w-8/12 h-2/6">
-                    <div className="flex items-end text-white text-3xl justify-center gap-4 py-2">
-                        <a href="https://github.com/Zetah07" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillGithub/></a>
-                        <img src="/images/testimonials/Johan.png" className="rounded-full object-center object-cover w-1/12 hover:scale-125" alt="Johan" />
-                        <a href="https://www.linkedin.com/in/Zetahdev/" className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillLinkedin/></a>
-                    </div>
-                    <div className="px-6">
-                        <p className="text-white text-sm text-center">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </p>
-                    </div>
-                </div> */}
+                {currentTestimonials.map((testimonial) => {
+                    return (
+                        <div id={testimonial.id} className="lg:w-8/12 h-2/6">
+                            <div className="flex items-end text-white text-5xl justify-center gap-4 py-2">
+                                <a href={testimonial.github} className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillGithub/></a>
+                                <img src={testimonial.img} className="border-4 rounded-full object-center object-cover w-2/12 hover:scale-125" alt="" />
+                                <a href={testimonial.linkedIn} className="hover:scale-125 hover:text-white transition ease-in-out delay-150"><AiFillLinkedin/></a>
+                            </div>
+                            <div className="px-6">
+                                <p className="text-white text-md text-center">{testimonial.decription}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+                <aside className="relative z-20 top-[30px]">
+                    <li className="text-white text-xl gap-4 flex">
+                        <ul className="hover:cursor-pointer hover:scale-150"><FiHexagon id="1" onClick={() => setCurrentPage(1)} className={currentPage === 1 ? "fill-white" : ""}/></ul>
+                        <ul className="hover:cursor-pointer hover:scale-150"><FiHexagon id="2" onClick={() => setCurrentPage(2)} className={currentPage === 2 ? "fill-white" : ""}/></ul>
+                        <ul className="hover:cursor-pointer hover:scale-150"><FiHexagon id="3" onClick={() => setCurrentPage(3)} className={currentPage === 3 ? "fill-white" : ""}/></ul>
+                    </li>
+                </aside>
             </main>
     )
 };
