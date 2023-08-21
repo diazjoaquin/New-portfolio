1184274d9753baa3ac0c2fcefe0a54115cba5313
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-20 22:46:31 -0300

    ft: add two new testimonials

--- new-portfolio/assets/testimonials.js
+++ new-portfolio/assets/testimonials.js
@@ -31,11 +31,20 @@ export const testimonials = [
         decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
 
-    // {
-    //     id: "testimonial-5",
-    //     img: "",
-    //     github: "",
-    //     linkedIn: "",
-    //     decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    // }
+    {
+        id: "testimonial-5",
+        img: "/images/testimonials/Fermin.jpg",
+        github: "https://github.com/fermin234",
+        linkedIn: "https://www.linkedin.com/in/fermin-solaberrieta-dev/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        id: "testimonial-6",
+        img: "/images/testimonials/Lucas.jpg",
+        github: "https://github.com/Lucascserrate",
+        linkedIn: "https://www.linkedin.com/in/lucascserrate/",
+        decription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
 ]
