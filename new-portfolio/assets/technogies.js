1fd636298fe051b8b850d635b7d7d741154cd78e
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-21 21:46:15 -0300

    ft: add new asset technologies

---
+++ new-portfolio/assets/technogies.js
@@ -0,0 +1,91 @@
+export const technologies = [
+    {
+        frontend: [
+            {
+                alt: "CSS",
+                img: "/images/technologies/CSS.png"
+            },
+            {
+                alt: "Chakra",
+                img: "/images/technologies/Chakra.png"
+            },
+            {
+                alt: "HTML",
+                img: "/images/technologies/HTML5.png"
+            },
+            {
+                alt: "React",
+                img: "/images/technologies/React.png"
+            },
+            {
+                alt: "Next",
+                img: "/images/technologies/Nextjs.png"
+            },
+            {
+                alt: "Tailwind",
+                img: "/images/technologies/Tailwind.png"
+            },
+            {
+                alt: "Sass",
+                img: "/images/technologies/sass.png"
+            },
+        ],
+
+        backend: [
+            {
+                alt: "Mongo",
+                img: "/images/technologies/Mongo.png"
+            },
+            {
+                alt: "Node",
+                img: "/images/technologies/Nodejs.png"
+            },
+            {
+                alt: "PosgreSQL",
+                img: "/images/technologies/postgresql.png"
+            },
+            {
+                alt: "Sequelize",
+                img: "/images/technologies/Sequelize.png"
+            },
+
+        ],
+        tools: [
+            {
+                alt: "NET",
+                img: "/images/technologies/NET.png"
+            },
+            {
+                alt: "Typescript",
+                img: "/images/technologies/Typescript.png"
+            },
+            {
+                alt: "Javascript",
+                img: "/images/technologies/Javscript.png"
+            },
+            {
+                alt: "Git",
+                img: "/images/technologies/Git.png"
+            },
+            {
+                alt: "Visual Studio",
+                img: "/images/technologies/Visual Studio.png"
+            },
+        ]
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+]
