d12725032bc821923095cc66a3e419eae446139d
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-29 21:59:35 -0300

    fix: add id to technologies array

--- new-portfolio/assets/technogies.js
+++ new-portfolio/assets/technogies.js
@@ -1,30 +1,37 @@
 export const technologies = {
         frontend: [
             {
+                id: 1,
                 alt: "CSS",
                 img: "/images/technologies/CSS.png"
             },
             {
+                id: 2,
                 alt: "Chakra",
                 img: "/images/technologies/Chakra.png"
             },
             {
+                id: 3,
                 alt: "HTML",
                 img: "/images/technologies/HTML5.png"
             },
             {
+                id: 4,
                 alt: "React",
                 img: "/images/technologies/React.png"
             },
             {
+                id: 5,
                 alt: "Next",
                 img: "/images/technologies/Nextjs.png"
             },
             {
+                id: 6,
                 alt: "Tailwind",
                 img: "/images/technologies/Tailwind.png"
             },
             {
+                id: 7,
                 alt: "Sass",
                 img: "/images/technologies/Sass.png"
             },
@@ -32,18 +39,22 @@ export const technologies = {
 
         backend: [
             {
+                id: 8,
                 alt: "Mongo",
                 img: "/images/technologies/Mongo.png"
             },
             {
+                id: 9,
                 alt: "Node",
                 img: "/images/technologies/Nodejs.png"
             },
             {
+                id: 10,
                 alt: "PosgreSQL",
                 img: "/images/technologies/postgresql.png"
             },
             {
+                id: 11,
                 alt: "Sequelize",
                 img: "/images/technologies/Sequelize.png"
             },
@@ -51,22 +62,27 @@ export const technologies = {
         ],
         tools: [
             {
+                id: 12,
                 alt: "NET",
                 img: "/images/technologies/NET.png"
             },
             {
+                id: 13,
                 alt: "Typescript",
                 img: "/images/technologies/Typescript.png"
             },
             {
+                id: 14,
                 alt: "Javascript",
                 img: "/images/technologies/JavaScript.png"
             },
             {
+                id: 15,
                 alt: "Git",
                 img: "/images/technologies/Git.png"
             },
             {
+                id: 16,
                 alt: "Visual Studio",
                 img: "/images/technologies/Visual Studio.png"
             },
