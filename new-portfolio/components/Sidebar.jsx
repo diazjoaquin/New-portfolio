2caddd6c8af152d6a797e0ddcd111ff2ebe05b1f
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-08-31 21:16:20 -0300

    ft: delete ig from sidebar

--- new-portfolio/components/Sidebar.jsx
+++ new-portfolio/components/Sidebar.jsx
@@ -4,8 +4,7 @@ export default function Sidebar () {
     return (
         <div className="mx-5 gap-4 text-gray-300 text-3xl fixed h-screen flex flex-col z-10 justify-center animate-fade animate-once animate-delay-[800ms] animate-ease-in">
             <a href="https://github.com/diazjoaquin" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiFillGithub/></a>
-            <a href="https://www.linkedin.com/in/diazjoaquin-dev/" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiFillLinkedin/></a>
-            <a href="https://www.instagram.com/diazjoaquin_/" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiOutlineInstagram/></a>
+            <a href="https://www.linkedin.com/in/diazjoaquin-dev/" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiFillLinkedin/></a> 
             <a href="https://wa.me/+541126792759" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiOutlineWhatsApp/></a>
             <a href="mailto:diaz.joaquin.e@gmail.com" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiOutlineMail/></a>
         </div>
