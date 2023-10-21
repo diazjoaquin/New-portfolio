be43553ffcf948408333cf5d35644826a2639861
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-10-21 15:11:22 -0300

    ft: add router push to home

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps}/>
-  }
+
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push('/home')
+  }, []);
+
+  return <Component {...pageProps}/>
+}
