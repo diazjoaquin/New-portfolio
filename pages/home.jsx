484b40d30bd4477be34235e43fcf6795aad0193f
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-11-14 01:28:05 -0300

    ft: add resonpisve design to homepage

--- pages/home.jsx
+++ pages/home.jsx
@@ -12,13 +12,13 @@ export default function Home () {
             <Nav/>
             <Sidebar/>
             <section className="w-screen flex items-center justify-center font-serif">
-                <div className="lg:h-4/5 lg:flex flex-row  gap-12 w-9/12 items-center px-12 absolute">
+                <div className="lg:h-4/5 lg:flex flex-row w-9/12 items-center px-6 md:px-2 absolute">
                     <article className="flex items-center justify-center">
-                        <img src="/images/profile.png" className="relative lg:w-full sm:w-[80%] transition ease-in-out delay-150 lg:grayscale sm:z-40 hover:grayscale-0 hover:cursor-pointer animate-fade-down animate-once animate-duration-[300ms] animate-ease-in transition ease-in-out delay-150" alt="profile" />
+                        <img src="/images/profile.png" className="relative w-full xl:w-[70%] transition ease-in-out delay-150 lg:grayscale sm:z-40 hover:grayscale-0 hover:cursor-pointer animate-fade-down animate-once animate-duration-[300ms] animate-ease-in transition ease-in-out delay-150" alt="profile" />
                     </article>
-                    <article className="w-full h-full flex flex-col justify-center text-white uppercase tracking-[.05em] z-20 pt-4">
-                        <p className="font-black font-sans pb-2 px-4 text-5xl md:text-6xl lg:text-7xl"><span className="italic text-4xl md:text-5xl text-teal-300">Full Stack </span> Web Developer</p>
-                        <h3 className="flex items-center text-xl font-sans px-4 italic font-medium animate-fade-up animate-once lowercase">A curious mind Whit great passion.</h3>
+                    <article className="max-w-screen h-full flex flex-col justify-center text-white uppercase tracking-[.05em] z-20 pt-4">
+                        <p className="font-black font-sans pb-2 md:px-4 text-4xl md:text-6xl lg:text-7xl"><span className="italic text-4xl md:text-5xl xl:text-7xl text-teal-300">Full Stack </span> Web Developer</p>
+                        <h3 className="flex items-center text-xl font-sans md:px-4 italic font-medium animate-fade-up animate-once lowercase">A curious mind Whit great passion.</h3>
                     </article>
                 </div>
             </section>
