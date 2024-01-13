cf6ddf371d7d6b9200f49fb884f2baccec30ec5e
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-01-12 23:23:56 -0300

    ft: add responsive design to testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -13,7 +13,7 @@ export default function Testimonials () {
 
     return (
         <section className="w-screen h-screen bg-zinc-900 flex items-center justify-center">
-            <main className="border-2 border-zinc-500 rounded-xl sm:w-8/12 lg:w-8/12 h-[80%] flex flex-col items-center justify-start pt-6 gap-24 animate-fade-down animate-duration-[1200ms] animate-ease-in backdrop-blur-sm backdrop-saturate-50 backdrop-brightness-125">
+            <main className="border-2 border-zinc-500 rounded-xl pb-2 w-8/12 h-auto lg:h-[80%] flex flex-col items-center justify-start pt-6 gap-24 animate-fade-down animate-duration-[1200ms] animate-ease-in backdrop-blur-sm backdrop-saturate-50 backdrop-brightness-125">
                 {currentTestimonials.map((testimonial) => {
                     return (
                         <div key={testimonial.id} className="lg:w-8/12 h-2/6 flex flex-col gap-2 animate-fade-down animate-duration-700 animate-ease-in animate-fill-both">
@@ -22,8 +22,8 @@ export default function Testimonials () {
                                 <img src={testimonial.img} className="border-4 rounded-full object-center object-cover w-2/12 hover:scale-125 transition ease-in-out delay-150" alt="" />
                                 <a href={testimonial.linkedIn} className="hover:scale-125 hover:text-teal-300 transition ease-in-out delay-150"><AiFillLinkedin/></a>
                             </div>
-                            <h3 className="text-teal-300 flex justify-center text-l font-bold font-sans">{testimonial.name}</h3>
-                            <div className="border-2 px-2 py-2 rounded-xl border-zinc-500 bg-zinc-800">
+                            <h3 className="text-teal-300 mx-2 text-center text-l font-bold font-sans">{testimonial.name}</h3>
+                            <div className="border-2 px-2 mx-2 py-2 rounded-xl border-zinc-500 bg-zinc-800">
                                 <p className="text-white text-md text-center">&quot;{testimonial.decription}&quot;</p>
                             </div>
                         </div>
