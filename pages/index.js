860dd8b8f5f11cb8e84ebae37d57396980aba54b
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-01-24 18:15:33 -0300

    fix: fix problem with module Home import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Nav from "@/components/Nav";
 import Sidebar from "@/components/Sidebar";
-import Home from "@/components/home";
+import Home from "@/components/Home";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Technologies from "@/components/Technologies";
