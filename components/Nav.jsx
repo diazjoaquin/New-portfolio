19d4f1ea1090da3b9e1b4cfdb889a6a86c136bb3
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-01-24 17:57:28 -0300

    ft: change navbar scrolling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,27 +4,14 @@ import { FaBars } from 'react-icons/fa';
 import MobileMenu from "./MobileMenu";
 import Link from 'next/link';
 
-import { useRouter } from "next/router";
-
 export default function Nav () {
 
     const [ showMobileMenu, setShowMobileMenu ] = useState(false);
     const [ activeLink, setActiveLink ] = useState('');
 
-    const router = useRouter();
-    const { pathname } = router;
-
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current)
     }, []);
-
-    useEffect(() => {
-        handleSelected(pathname);
-    }, []);
-
-    const handleSelected = (pathname) => {
-        setActiveLink(pathname);
-    };
 
     return (
         <nav className="w-screen h-20 fixed text-white flex items-center font-mono text-l uppercase z-50">
@@ -34,17 +21,17 @@ export default function Nav () {
             </div>
             <div className="w-5/12 hidden lg:flex">
                 <ul className="w-full flex justify-evenly hover:text-gray-300 py-2">
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/home" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
-                        <Link href="/home">Home</Link>
+                    <li className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                        <Link href="#home">Home</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/about" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
-                        <Link href="/about">About</Link>
+                    <li className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                        <Link href="#about">About</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/portfolio" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
-                        <Link href="/portfolio">Portfolio</Link>
+                    <li className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                        <Link href="#portfolio">Portfolio</Link>
                     </li>
-                    <li onClick={() => handleSelected(pathname)} id={activeLink === "/contact" ? "active" : ""} className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
-                        <Link href="/contact">Contact</Link>
+                    <li className="after:block after:h-[3px] after:w-100 after:bg-teal-500 after:scale-x-0 element hover:cursor-pointer hover:text-white">
+                        <Link href="#contact">Contact</Link>
                     </li>
                 </ul>
             </div>
